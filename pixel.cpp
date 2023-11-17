cb73c7757c79fe818b1e531f09d18f94950301b0
bgk <bogyeongkim614@gmail.com>
2023-11-17 20:44:06 +0900

    Update pixel.cpp

--- pixel.cpp
+++ pixel.cpp
@@ -1,6 +1,8 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 #include <stdio.h>
+#include <vector>
+#include <string>
 
 using namespace cv;
 using namespace std;
@@ -8,69 +10,71 @@ using namespace std;
 void getGreen(Mat src, vector<int>& all_pixel, vector<int>& green_pixel, vector<double>& green_percent, vector<double>& green_progress);
 void calculateGreenPercent(Mat src, int& gCount, vector<double>& green_percent);
 void calculateGreenProgress(vector<int>& green_pixel, vector<double>& green_progress); 
-void drawBarGraph(const vector<double>& data, const string& graphTitle);
+string saveGreenImage(Mat& resultImg, int& index);
 
-int main() 
-{
-    // 값을 저장할 벡터
-    vector<int> all_pixel;
-    vector<int> green_pixel;
-    vector<double> green_percent;
-    vector<double> green_progress;
+int i = 0;  // 이미지 번호
 
-    Mat src0 = imread("강원도0.png");   // 불 나기 전   2018.07
-    Mat src1 = imread("강원도1.png");   // 불난 직후    2019.04
-    Mat src2 = imread("강원도2.png");   // 1년 후      2020.02
-    Mat src3 = imread("강원도3.png");   // 현재        2020.10
-    
-    // 이미지 저장 벡터
-    vector<Mat> images = {src0, src1, src2, src3};
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cerr << "Usage: " << argv[0] << " <image1> <image2> ..." << endl;
+        return 1;
+    }
 
-    for (const auto& img : images) {
-        if(img.empty()) {
-            cerr << "Image load failed." << endl;
+    vector<int> all_pixel, green_pixel;
+    vector<double> green_percent, green_progress;
+    vector<string> savedImages; 
+
+    for (int i = 1; i < argc; i++) {
+        Mat img = imread(argv[i]);
+        if (img.empty()) {
+            cerr << "Image load failed: " << argv[i] << endl;
             return -1;
         }
-    }
 
-    for (const auto& img : images) {
         getGreen(img, all_pixel, green_pixel, green_percent, green_progress);
-    }
-    
-    // 그래프 그리기
-    drawBarGraph(vector<double>(all_pixel.begin(), all_pixel.end()), "All Pixels");
-    drawBarGraph(vector<double>(green_pixel.begin(), green_pixel.end()), "Green Pixels");
-    drawBarGraph(green_percent, "Green Percent");
-    drawBarGraph(vector<double>(green_progress.begin(), green_progress.end()), "Green Progress");
-
-    // 결과 출력
-    for(int i = 0; i < all_pixel.size(); i++) {
-        if (i == 0) 
-            cout << "== Before a wildfire ==" << endl;
-        else if (i == 1) 
-            cout << "== After a wildfire ==" << endl;
-        else if (i == all_pixel.size() - 1) 
-            cout << "==     Present     ==" << endl;
-        else 
-            cout << "== " << i << " Years later ==" << endl;
-
-        cout << "   All pixel : " << all_pixel[i] << endl;
-        cout << "   Green pixel : " << green_pixel[i] << endl;
-        cout << "   Green percent " << green_percent[i] << "%" << endl;
+        savedImages.push_back(saveGreenImage(img, i));
     }
 
-    for(int i = 0; i < green_progress.size(); i++) {
-        cout << "   Green progress : " << green_progress[i] << "%" << endl;
-    }
+        // JSON 형식으로 결과 출력
+        cout << "{";
 
-    waitKey();
-    destroyAllWindows();
+        cout << "\"all_pixel\": [";
+        for (size_t i = 0; i < all_pixel.size(); ++i) {
+            cout << all_pixel[i] << (i < all_pixel.size() - 1 ? ", " : "");
+        }
+        cout << "],";
+
+        cout << "\"green_pixel\": [";
+        for (size_t i = 0; i < green_pixel.size(); ++i) {
+            cout << green_pixel[i] << (i < green_pixel.size() - 1 ? ", " : "");
+        }
+        cout << "],";
+
+        cout << "\"green_percent\": [";
+        for (size_t i = 0; i < green_percent.size(); ++i) {
+            cout << green_percent[i] << (i < green_percent.size() - 1 ? ", " : "");
+        }
+        cout << "],";
+
+        cout << "\"green_progress\": [";
+        for (size_t i = 0; i < green_progress.size(); ++i) {
+            cout << green_progress[i] << (i < green_progress.size() - 1 ? ", " : "");
+        }
+        cout << "],"; 
+
+        // 저장된 이미지 경로 출력
+        cout << "\"savedImages\": [";
+        for (size_t i = 0; i < savedImages.size(); ++i) {
+            cout << "\"" << savedImages[i] << "\"" << (i < savedImages.size() - 1 ? ", " : "");
+        }
+        cout << "]";
+
+        cout << "}";
 
     return 0;
 }
 
-int i = 0;  // 이미지 번호
-
+// 사진의 녹색 픽셀 추출
 void getGreen(Mat src, vector<int>& all_pixel, vector<int>& green_pixel, vector<double>& green_percent, vector<double>& green_progress) {
     Mat src_hsv;                                            // 이미지를 HSV 색 공간으로 변환
     cvtColor(src, src_hsv, COLOR_BGR2HSV);
@@ -116,9 +120,7 @@ void getGreen(Mat src, vector<int>& all_pixel, vector<int>& green_pixel, vector<
     calculateGreenPercent(src, gCount, green_percent);
     calculateGreenProgress(green_pixel, green_progress);
 
-    // 결과 이미지를 표시
-    imshow("Original Img" + to_string(i), src);
-    imshow("Result Img" + to_string(i), resultImg);
+    string result = saveGreenImage(resultImg, i);
 
     i++;
 }
@@ -142,39 +144,9 @@ void calculateGreenProgress(vector<int> &green_pixel, vector<double> &green_prog
     }
 }
 
-// 그래프 그리기
-void drawBarGraph(const vector<double>& data, const string& graphTitle) {
-    int graphHeight = 400;
-    int graphWidth = 400;
-    int barWidth = cvRound((double)graphWidth / data.size());
-    int axisThick = 2;
-
-    Mat graphImg(graphHeight + 20, graphWidth + 40, CV_8UC3, Scalar(255, 255, 255));
-
-    // 데이터의 최대값을 찾아서 그래프의 스케일을 결정
-    double maxVal = *max_element(data.begin(), data.end());
-
-    // 막대 그리기
-    for (size_t i = 0; i < data.size(); i++) {
-        int barHeight = cvRound((data[i] / maxVal) * (graphHeight - 150));
-        rectangle(graphImg, Point(i * barWidth + 21, graphHeight - barHeight),
-                  Point((i + 1) * barWidth, graphHeight), Scalar(100, 200, 100), FILLED);
-    }
-
-    // x축과 y축
-    line(graphImg, Point(20, 0), Point(20, graphHeight), Scalar(0, 0, 0), axisThick);
-    line(graphImg, Point(20, graphHeight), Point(graphWidth + 20, graphHeight), Scalar(0, 0, 0), axisThick);
-
-    // 축 레이블 추가
-    for (size_t i = 0; i < data.size(); i++) {
-        int num = cvRound(data[i]);
-        string text = to_string(num);
-        putText(graphImg, text, Point(i * barWidth + 25, graphHeight - 5), FONT_HERSHEY_PLAIN, 1, Scalar(0, 0, 255));
-    }
-
-    // 그래프 제목 추가
-    putText(graphImg, graphTitle, Point(20, 15), FONT_HERSHEY_PLAIN, 1, Scalar(255, 0, 0));
-
-    // 그래프 이미지
-    imshow(graphTitle, graphImg);
+// 초록색 픽셀만 추출한 이미지를 저장하고 경로를 반환하는 부분
+string saveGreenImage(Mat& resultImg, int& index) {
+    string outputPath = "./greenArea_" + to_string(index) + ".jpg";
+    imwrite(outputPath, resultImg);     // 결과 이미지를 파일로 저장
+    return outputPath;                  // 저장된 이미지의 경로 반환
 }
