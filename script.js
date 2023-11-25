6362f77d26394450a44d081dad531e149106601f
bgk <bogyeongkim614@gmail.com>
2023-11-25 16:21:31 +0900

    Feat: 폴더 이미지 삭제 및 차트 그리기

--- script.js
+++ script.js
@@ -1,77 +1,146 @@
+document.getElementById('Image').addEventListener('click', function() {
+    fetch('/delete-images', { method: 'POST' })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Images deleted:', data.message);
+        })
+        .catch(error => console.error('Error:', error));
+});
+
 document.getElementById('uploadForm').addEventListener('submit', async function(e) {
     e.preventDefault();
     const formData = new FormData(this);
-    const response = await fetch('/upload', {
-        method: 'POST',
-        body: formData
-    });
+    const response = await fetch('/uploads', { method: 'POST', body: formData });
 
     const result = await response.json();
     if (result.success) {
-        // JSON 문자열을 객체로 변환
-        const data = JSON.parse(result.result);
-
-        // 결과를 웹페이지에 표시
-        displayResults(data);
+        const data = JSON.parse(result.result); 
+        displayResults(data);                   
+        displayImages(data.green_pixel.length);
+        chart(data);
     } else {
         console.error('Error in processing images');
     }
 });
 
 function displayResults(data) {
-    document.getElementById('allPixel').innerText = 'All Pixels: ' + data.all_pixel;
-    document.getElementById('greenPixel').innerText = 'Green Pixels: ' + data.green_pixel;
-    document.getElementById('greenPercent').innerText = 'Green Percent: ' + data.green_percent + '%';
-    document.getElementById('greenProgress').innerText = 'Green Progress: ' + data.green_progress + '%';
-
-    // 새로 추가된 부분: 저장된 이미지 경로를 이용하여 이미지 표시
-    const imagePaths = data.savedImages;
-    for (let i = 0; i < imagePaths.length; i++) {
-        document.getElementById(`imagePreview${i+1}`).querySelector('img').src = imagePaths[i];
-    }
+    document.getElementById('allPixel').innerText = 'All Pixels: ' + data.all_pixel
+    document.getElementById('greenPixel').innerText = 'Green Pixels: ' + data.green_pixel.join(' / ');
+    document.getElementById('greenPercent').innerText = 'Green Similar Percent: ' + data.green_similar_percent.join(' / ');
+    document.getElementById('greenProgress').innerText = 'Green Progress: ' + data.green_progress.join(' / ');
 }
 
+function displayImages(greenPixelCount) {
+    const srcImageContainer = document.querySelector('.srcImageContainer');
+    const greenImageContainer = document.querySelector('.greenImageContainer');
+    const changeImageContainer = document.querySelector('.changeImageContainer');
+    
+    srcImageContainer.innerHTML = '';
+    greenImageContainer.innerHTML = '';
+    changeImageContainer.innerHTML = '';
 
-function previewImages() {
-    // 이미지 미리보기를 위한 함수
-    var images = document.querySelectorAll('input[type=file]');
+    for (let i = 1; i < greenPixelCount + 1; i++) {
+        const srcImg = new Image();
+        srcImg.src = `./srcImage/srcImage_${i}.jpg`;
+        srcImageContainer.appendChild(srcImg);
 
-    images.forEach(image => {
-        if (image.files && image.files[0]) {
-            var reader = new FileReader();
-            reader.onload = function (e) {
-                // 이미지에 해당하는 미리보기 엘리먼트의 src 속성을 설정
-                var previewId = image.id + 'Display';
-                document.getElementById(previewId).src = e.target.result;
-                document.getElementById(previewId).style.display = 'block';
-            };
+        const greenImg = new Image();
+        greenImg.src = `./greenPixelImage/greenArea_${i}.jpg`;
+        greenImageContainer.appendChild(greenImg);
 
-            // 파일 읽기
-            reader.readAsDataURL(image.files[0]);
+        if (i < greenPixelCount) {
+            const changeImg = new Image();
+            changeImg.src = `./changeImage/changeArea_${i}_${i+1}.jpg`;
+            if (i === 1) changeImg.style.marginLeft = '148px'; 
+            changeImageContainer.appendChild(changeImg);
+        
         }
-    });
+    }
 }
 
-// 각 파일 입력 필드에 미리보기 기능 연결
-document.getElementById('pastImage').addEventListener('change', previewImages);
-document.getElementById('fireImage').addEventListener('change', previewImages);
-document.getElementById('1YearlaterImage').addEventListener('change', previewImages);
-document.getElementById('presentImage').addEventListener('change', previewImages);
+var greenPixelChartInstance;
+var similarPercentChartInstance;
+var greenProgressChartInstance;
 
-// script.js
-document.getElementById('uploadForm').addEventListener('submit', async function(e) {
-    e.preventDefault();
-    const formData = new FormData(this);
-    const response = await fetch('/upload', {
-        method: 'POST',
-        body: formData
-    });
-
-    const result = await response.json();
-    if (result.success) {
-        // 결과를 웹페이지에 표시
-        displayResults(result.result);
-    } else {
-        console.error('Error in processing images');
+function chart(data) {
+    if (greenPixelChartInstance) {
+        greenPixelChartInstance.destroy();
     }
-});
+    if (similarPercentChartInstance) {
+        similarPercentChartInstance.destroy();
+    }
+    if (greenProgressChartInstance) {
+        greenProgressChartInstance.destroy();
+    }
+
+    const greenPixelChart = document.getElementById('greenPixelChart').getContext('2d');
+    greenPixelChartInstance = new Chart(greenPixelChart, {
+        type: 'line', 
+        data: {
+            labels: data.green_pixel.map((_, i) => `Image ${i+1}`), 
+            datasets: [{
+                    label: 'Green Pixels',
+                    data: data.green_pixel, 
+                    backgroundColor: 'rgba(45, 192, 72)', 
+                    borderColor: 'rgba(45, 192, 72)', 
+                    borderWidth: 0.5,
+                    lineTension: 0.2
+                }, {
+                    type: 'bar',
+                    label: 'Green Pixels',
+                    data: data.green_pixel, 
+                    backgroundColor: 'rgba(30, 50, 30, 0.1)', 
+                    borderWidth: 0.5,
+                }]
+        },
+        options: {scales: {y: {beginAtZero: true}}}
+    })
+    const similarPercentChart = document.getElementById('similarPercentChart').getContext('2d');
+    similarPercentChartInstance = new Chart(similarPercentChart, {
+        type: 'line', 
+        data: {
+            labels: data.green_similar_percent.map((_, i) => `Image ${i+2}`), 
+            datasets: [{
+                    label: 'Green Similar Percent',
+                    data: data.green_similar_percent, 
+                    backgroundColor: 'rgba(107, 102, 155)', 
+                    borderColor: 'rgba(107, 102, 155)', 
+                    borderWidth: 0.5,
+                    lineTension: 0.2
+                }, {
+                    type: 'bar',
+                    label: 'Green Similar Percent',
+                    data: data.green_similar_percent, 
+                    backgroundColor: 'rgba(30, 50, 30, 0.1)', 
+                    borderWidth: 0.5,
+                }]
+        },
+        options: {scales: {y: {beginAtZero: true }}}
+        })
+    const greenProgressChart = document.getElementById('greenProgressChart').getContext('2d');
+    greenProgressChartInstance = new Chart(greenProgressChart, {
+        type: 'line', 
+        data: {
+        labels: data.green_progress.map((_, i) => `Image ${i+2}`), 
+        datasets: [{
+                label: 'Green Progress',
+                data: data.green_progress, 
+                backgroundColor: 'rgba(255, 114, 0)', 
+                borderColor: 'rgba(255, 114, 0)', 
+                borderWidth: 0.5,
+                lineTension: 0.2
+                }, {   
+                type: 'bar', 
+                label: 'Green Progress',
+                data: data.green_progress, 
+                backgroundColor: 'rgba(30, 50, 30, 0.1)', 
+                borderWidth: 0.5,
+                }]
+            },
+            options: {
+                scales: {
+                    y: {beginAtZero: true }
+                }
+            }
+        })
+}
